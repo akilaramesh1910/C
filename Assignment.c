5d0dd90ced9684ecac6bf57c0e141a74e709d4e2
akilaramesh1910 <130809341+akilaramesh1910@users.noreply.github.com>
2024-01-31 23:36:59 +0530

    first commit

---
+++ Assignment.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    //IMPLICIT
+
+    // int a,b;
+    // float c;
+    // a=5,b=2;
+    // c=a/b;
+    // printf("The value of c is %f",c);
+    // return 0;
+
+    // EXPLICIT
+
+    int a,b;
+    float c;
+    a=5,b=2;
+    c=(float)a/b;
+    printf("The value of c is %f",c);
+    return 0;
+}
